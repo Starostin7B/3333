7d54a79e30c18990afd9ba395c753be461370d0b
12345 <82112878+Starostin7B@users.noreply.github.com>
2026-09-02 11:41:12 +0300

    Add files via upload

---
+++ 123.py
@@ -0,0 +1,16 @@
+print ("vedi 1 cifry")
+a = float(input())
+print ("vedi + - * /")
+qwe = input()
+print ("vedi 2 cifry")
+b = float(input())
+if qwe == "+":
+    print (a+b)
+elif qwe == "-":
+    print (a-b)
+elif qwe == "*":
+    print (a*b)
+elif qwe == "/":
+    print (a/b)
+else:
+    print ("neizvesnaya comada")
